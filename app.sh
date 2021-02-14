7ba262dfbacad55bd7f06ab5725815e30ff97360
Muhammad Ghifari <ghifari9567@gmail.com>
2021-02-14 13:47:12 +0700

    Added app.sh for M1

---
+++ app.sh
@@ -0,0 +1,5 @@
+# Desktop
+chmod +x gradlew
+export PATH=/opt/homebrew/bin:$PATH
+./gradlew assembleDebug && ./gradlew installDevelopmentDebug
+adb shell am start -n com.example.animatedsplash/.MainActivity
